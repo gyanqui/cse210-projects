ff8a48634e8e83b253ce420316870a36ea82bf73
gyanqui <gyanqui3@gmail.com>
2023-05-08 18:18:55 -0500

    Adding Design assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Data Analytic";
+        job3._company = "Metadata";
+        job3._startYear = 2023;
+        job3._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,182 @@
 using System;
+// using System.Collections.Generic;
+// using System.IO;
 
-class Program
+namespace DailyJournal
 {
-    static void Main(string[] args)
+    class Entry
     {
-        Console.WriteLine("Hello Develop02 World!");
+        public string Prompt { get; set; }
+        public string Response { get; set; }
+        public string Date { get; set; }
+
+        public Entry(string prompt, string response, string date)
+        {
+            Prompt = prompt;
+            Response = response;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"Date: {Date} - Prompt: {Prompt} \n{Response}\n";
+        }
     }
-}
+
+    class Journal
+    {
+        private List<Entry> entries;
+
+        public Journal()
+        {
+            entries = new List<Entry>();
+        }
+
+        public void AddEntry(string prompt)
+        {
+            Console.Write(prompt + " ");
+            string response = Console.ReadLine();
+            string date = DateTime.Now.ToString("MM/dd/yyyy");
+            entries.Add(new Entry(prompt, response, date));
+        }
+
+        public void DisplayEntries()
+        {
+            foreach (Entry entry in entries)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+        public void SaveToFile()
+        {
+            Console.Write("Create a Filename: ");
+            string filename = Console.ReadLine();
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Entry entry in entries)
+                {
+                    writer.WriteLine($"{entry.Date},{entry.Prompt},{entry.Response}");
+                }
+            }
+            Console.WriteLine("Journal saved to file.");
+        }
+
+        public void LoadFromFile()
+        {
+            Console.Write("Enter filename: ");
+            string filename = Console.ReadLine();
+            entries.Clear();
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] fields = reader.ReadLine().Split(',');
+                    string date = fields[0];
+                    string prompt = fields[1];
+                    string response = fields[2];
+                    entries.Add(new Entry(prompt, response, date));
+                }
+            }
+            Console.WriteLine("Journal loaded from file.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+
+            // List of prompts
+            List<string> prompts = new List<string>
+            {
+                "Did anything unexpected happen today that surprised you? ",
+                "What was the most challenging thing you faced today and how did you overcome it? ",
+                "What was the best moment of your day and why? ",
+                "Who did you share a special moment with today? ",
+                "Did you discover anything new or learn something interesting today? ",
+                "Did someone make you feel special or make you smile today?" ,
+                "Did you achieve any success in your work, studies or personal projects today? ",
+                "Did you do anything today that took you out of your comfort zone? ",
+                "What would you like to do more of in your daily life and how do you plan to achieve it? ",
+                "What do you most wish for tomorrow? ",
+                "How did you show love or compassion towards others today, inspired by your faith? ",
+                "Did you receive any unexpected blessings today that you attribute to your faith? If so, can you describe them? ",
+                "What was the most inspiring thing you heard or read today that strengthened your faith? ",
+                "How did you feel God's presence in your life today? ",
+                "What was the most meaningful spiritual experience you had today? ",
+                "How did you demonstrate your faith through actions or choices you made today? ",
+            };
+
+            bool quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("\nWelcome to the Journal Program");
+                Console.WriteLine("Please Select one of the following choices:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display journal");
+                Console.WriteLine("3. Save Journal in a file");
+                Console.WriteLine("4. Load Journal from a file");
+                Console.WriteLine("5. Quit");
+
+                Console.Write("\nWhat would you like to do? ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        journal.AddEntry(prompts[new Random().Next(prompts.Count)]);
+                        break;
+                    case "2":
+                        journal.DisplayEntries();
+                        break;
+                    case "3":
+                        journal.SaveToFile();
+                        break;
+                    case "4":
+                        journal.LoadFromFile();
+                        break;
+                    case "5":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            } Console.WriteLine();
+        }   
+    }
+} 
+
+/*
+Program Diagram 
+                            +-----------------+
+                            |     Program     |
+                            +-----------------+
+                            |    -Journal     |
+                            +-----------------+
+                                        |
+                                        |
+                                        |
+                            +-----------+-----------+
+                            |                       |
+                    +------+--------+           +-------+-----+
+                    |  PromptList   |           |     Entry   |
+                    +-----------------------+ +----------------+
+                    | -prompts: list of str | | -prompt: str   |
+                    |                       | | -response: str |
+                    | +get_prompt(): str    | | -date: str     |
+                    +-----------------------+ +----------------+
+                                            |
+                                            |
+                        +-------------+-------------------+
+                        |             |                   |
+            +---------+-----+ +-----+---------+     +------------+
+            |   SaveToFile  | |  LoadFromFile |     |  Display   |
+            +----------------+ +----------------+   +-------------------------+
+            | -filename: str | | -filename: str |   | -journal: list of Entry |
+            |                | |                |   |                         |
+            | +save(): None  | | +load(): None  |   | +display(): None        |
+            +----------------+ +----------------+   +-------------------------+     
+*/
