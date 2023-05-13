f688ce764990608cf28d43aeca04ec08026eb247
gyanqui <gyanqui3@gmail.com>
2023-05-13 05:51:01 -0500

    Improve the process of saving and loading

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -26,18 +26,21 @@ namespace DailyJournal
                 Console.WriteLine(entry.ToString());
             }
         }
-
         public void SaveToFile()
         {
             Console.Write("Enter Filename: ");
             string filename = Console.ReadLine();
+
             using (StreamWriter writer = new StreamWriter(filename))
             {
+                writer.WriteLine("Date,Prompt,Response");
+
                 foreach (Entry entry in entries)
                 {
-                    writer.WriteLine($"{entry.GetDate()},{entry.GetPrompt()},{entry.GetResponse()}");
+                    writer.WriteLine($"{entry.GetDate()},{entry.GetPrompt().Replace(",", ",,")},{entry.GetResponse().Replace(",", ",,")}");
                 }
             }
+
             Console.WriteLine("Saved");
         }
 
@@ -46,17 +49,24 @@ namespace DailyJournal
             Console.Write("Enter filename: ");
             string filename = Console.ReadLine();
             entries.Clear();
+
             using (StreamReader reader = new StreamReader(filename))
             {
+                string headerLine = reader.ReadLine(); // Read and discard the header line
+
                 while (!reader.EndOfStream)
                 {
-                    string[] fields = reader.ReadLine().Split(',');
+                    string entryLine = reader.ReadLine();
+                    string[] fields = entryLine.Split(',');
+
                     string date = fields[0];
-                    string prompt = fields[1];
-                    string response = fields[2];
+                    string prompt = fields[1].Replace(",,", ",");
+                    string response = fields[2].Replace(",,", ",");
+
                     entries.Add(new Entry(prompt, response, date));
                 }
             }
+
             Console.WriteLine("Loaded");
         }
     }
