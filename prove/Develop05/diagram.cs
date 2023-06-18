ad436686707dc1dc7317a60135bd2b03daa5a96e
gyanqui <gyanqui3@gmail.com>
2023-06-18 00:56:10 -0500

    Add diagram for dev5

--- prove/Develop05/diagram.cs
+++ prove/Develop05/diagram.cs
@@ -1,34 +1,84 @@
 /*
-        ----------------------------------------------
-        |                  Goal                      |
-        ----------------------------------------------
-        | - name: string                             |
-        | - description: string                      |
-        ----------------------------------------------
-        | + recordEvent(): int                       |
-        | + isComplete(): bool                       |
-        ---------------------------------------------
-                |           |                |
-                |           |                |
-                |           |                |
-                |           |                |
-            SimpleGoal   EternalGoal    ChecklistGoal
-        ---------------------------------------------
-        | - value: int                              |
-        ---------------------------------------------
-        | + recordEvent(): int                      |
-        | + isComplete(): bool                       |
-        ---------------------------------------------
 
-                |           |                |
-                |           |                |
-                |           |                |
-            ProgramGoal  ScriptureGoal  TempleGoal
-        ---------------------------------------------
-        | - bonus: int                              |
-        | - completedTimes: int                      |
-        ---------------------------------------------
-        | + recordEvent(): int                       |
-        | + isComplete(): bool                       |
-        ---------------------------------------------
-*/ 
++----------------------------------+
+|           Program                |
++----------------------------------+
+| - welcome: string                |
++----------------------------------+
+| + Start()                        |
+| + Quit()                         |
+| + MainMenu()                     |
+| + GoalMenu()                     |
+| + Goal()                         |
+| + CreateGoal(params)             |
+| + ListGoal(params)               |
+| + SaveGoal()                     |
+| + LoadGoal()                     |
+| + RecordEvent()                  |
++----------------------------------+
+                ^
+                |
+                |
+                |
+                v
++----------------------------------+
+|             Goal                 |
++----------------------------------+
+| - name: string                   |
+| - points: int                    |
+| - status: string                 |
+| - goals: List<Entry>             |
++----------------------------------+
+| + Goal()                         |
+| + CreateGoal(params): returnType |
+| + ListGoal(params)               |
+| + SaveGoal()                     |
+| + LoadGoal()                     |
+| + RecordEvent()                  |
++----------------------------------+
+                ^
+                |                           +----------------------------------+
+                |                           |           SimpleGoal              |
+                +-------------------------->|----------------------------------+
+                |                           | - points: int                    |
+                |                           | - description: string            |
+                |                           | - status: string                 |
+                |                           +----------------------------------+
+                |                           | + SimpleGoal(params): returnType |
+                |                           | + CreateGoal(params): returnType |
+                |                           | + GetDescription(params)         |
+                |                           | + CalculatePoints()              |
+                |                           +----------------------------------+
+                |
+                |                           +----------------------------------+
+                |                           |          EternalGoal              |
+                +-------------------------->|----------------------------------+
+                |                           | - points: int                    |
+                |                           | - description: string            |
+                |                           | - status: string                 |
+                |                           +----------------------------------+
+                |                           | + EternalGoal(params): returnType|
+                |                           | + CreateGoal(params): returnType |
+                |                           | + GetDescription(params)         |
+                |                           | + CalculatePoints()              |
+                |                           +----------------------------------+
+                |
+                |
+                |
+                |                           +----------------------------------+
+                |                           |         ChecklistGoal            |
+                +-------------------------->|----------------------------------+
+                                            | - points: int                    |
+                                            | - description: string            |
+                                            | - bonus: int                     |
+                                            | - frequency: int                 |
+                                            | - status: string                 |
+                                            +----------------------------------+
+                                            | + ChecklistGoal(params): returnType |
+                                            | + CreateGoal(params): returnType |
+                                            | + GetDescription(params)         |
+                                            | + CalculatePoints()              |
+                                            | + CalculateBonus()               |
+                                            +----------------------------------+
+
+*/
