3c3ee7f5590350bd7ef43d7e3dfb03b09ac733a8
gyanqui <gyanqui3@gmail.com>
2023-07-15 18:05:28 -0500

    Complete final project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,29 +1,9 @@
 using System;
 
-public class Running : Exercise
+class Program
 {
-    private float _distance;
-
-    public Running(string date, float minutes, float distance) : base(date, minutes)
+    static void Main(string[] args)
     {
-        _name = "Running";
-        _distance = distance;
-    }
-
-    public override float CalculateDistance()
-    {
-        return _distance;
-    }
-
-    public override float CalculateSpeed()
-    {
-        float speed = (_distance / _minutes) * 60;
-        return Math.Round(speed, 1);
-    }
-
-    public override float CalculatePace()
-    {
-        float pace = _minutes / _distance;
-        return Math.Round(pace, 1);
+        Console.WriteLine("Hello FinalProject World!");
     }
 }
