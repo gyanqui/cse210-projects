256d98ea301dc5a0c1f8e740d4ef8b72473f71e9
gyanqui <gyanqui3@gmail.com>
2023-07-15 16:38:13 -0500

    Fix length to display in seconds

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -9,7 +9,7 @@ class Program
         List<Video> videosList = new List<Video>();
 
         // Video 1
-        Video video1 = new Video("Living in the Heart of Peru: Exploring Ancient Ruins", "Juan Perez Garcia", 8.45);
+        Video video1 = new Video("Living in the Heart of Peru: Exploring Ancient Ruins", "Juan Perez Garcia", 480);
 
         Comment video1Comment1 = new Comment("Luisa", "This video is truly inspiring! I love how you explore the ancient ruins and share their history.");
         Comment video1Comment2 = new Comment("Carlos", "Your content always surprises me. I'm glad you're showcasing the beauty of our country.");
@@ -22,7 +22,7 @@ class Program
         videosList.Add(video1);
 
         // Video 2
-        Video video2 = new Video("Hidden Gems of Peru: Uncovering Mysteries in the Andes", "Jhon Mercedes Palomino", 16.56);
+        Video video2 = new Video("Hidden Gems of Peru: Uncovering Mysteries in the Andes", "Jhon Mercedes Palomino", 975);
 
         Comment video2Comment1 = new Comment("Diego", "What an incredible adventure! I love how you showcase the mysteries of the Andes and invite us to discover more about our history.");
         Comment video2Comment2 = new Comment("Valeria", "Your videos always transport me to new places. Thank you for sharing these hidden gems.");
@@ -35,7 +35,7 @@ class Program
         videosList.Add(video2);
 
         // Video 3
-        Video video3 = new Video("Peruvian Cuisine: A Gastronomic Adventure", "Miriam Velasquez Soto", 19.28);
+        Video video3 = new Video("Peruvian Cuisine: A Gastronomic Adventure", "Miriam Velasquez Soto", 650);
 
         Comment video3Comment1 = new Comment("Gabriel", "My mouth was watering while watching this video! Our cuisine is truly unique and delicious.");
         Comment video3Comment2 = new Comment("Renata", "I love how you present our traditional dishes. I definitely need to try them all.");
